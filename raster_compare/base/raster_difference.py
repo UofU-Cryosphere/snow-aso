a856804021b226fc87d11f840f2036e1810a10f2
Joachim Meyer <j.meyer@utah.edu>
2018-08-16 14:32:35 -0600

    RasterDifference class - Add methods for slope and aspect difference.

--- raster_compare/base/raster_difference.py
+++ raster_compare/base/raster_difference.py
@@ -3,27 +3,41 @@ from base.raster_file import RasterFile
 
 
 class RasterDifference(object):
-    UPPER_FILTER = 20
-    LOWER_FILTER = -10
+    ELEVATION_UPPER_FILTER = 20
+    ELEVATION_LOWER_FILTER = -10
 
     BIN_WIDTH = 10  # 10m
 
     def __init__(self, lidar, sfm):
         self.lidar = lidar if type(lidar) is RasterFile else RasterFile(lidar)
         self.sfm = sfm if type(sfm) is RasterFile else RasterFile(sfm)
+        self._aspect = None
         self._elevation = None
+        self._slope = None
+
+    @property
+    def aspect(self):
+        if self._aspect is None:
+            self._aspect = self.sfm.aspect - self.lidar.aspect
+        return self._aspect
 
     @property
     def elevation(self):
         if self._elevation is None:
             self._elevation = self.sfm.elevation - self.lidar.elevation
             self._elevation.mask[
-                self._elevation > self.UPPER_FILTER
-                ] = self.UPPER_FILTER
+                self._elevation > self.ELEVATION_UPPER_FILTER
+                ] = self.ELEVATION_UPPER_FILTER
             self._elevation.mask[
-                self._elevation < self.LOWER_FILTER
-                ] = self.LOWER_FILTER
+                self._elevation < self.ELEVATION_LOWER_FILTER
+                ] = self.ELEVATION_LOWER_FILTER
         return self._elevation
+
+    @property
+    def slope(self):
+        if self._slope is None:
+            self._slope = self.sfm.slope - self.lidar.slope
+        return self._slope
 
     def min_for_attr(self, attr):
         return min(
