a182fc484c0df6630d20d554d22d48fa80578a63
Joachim Meyer <j.meyer@utah.edu>
2018-09-05 11:39:06 -0600

    Raster compare - Add RasterDifference to init

--- raster_compare/base/__init__.py
+++ raster_compare/base/__init__.py
@@ -1,7 +1,9 @@
 from .plot_base import PlotBase
+from .raster_difference import RasterDifference
 from .raster_file import RasterFile
 
 __all__ = [
     'PlotBase',
-    'RasterFile'
+    'RasterDifference',
+    'RasterFile',
 ]
