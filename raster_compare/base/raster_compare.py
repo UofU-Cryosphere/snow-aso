710fdc4bf204277bc3f3fad91270f8d04d31334f
Joachim Meyer <j.meyer@utah.edu>
2018-09-06 13:21:18 -0600

    Raster compare - Create RasterCompare class.
    This class prepares the comparison by checking that all paths and input files
    exist and crops raster files to shape when given.
    Reduces complexity for plot_all.py and RasterFile class.

---
+++ raster_compare/base/raster_compare.py
@@ -0,0 +1,96 @@
+import os
+import sys
+
+import gdal
+
+from base.raster_file import RasterFile
+
+
+class RasterCompare(object):
+    def __init__(self, **kwargs):
+        self.base_path = kwargs['base_path']
+        self.output_path = kwargs['output_path']
+        self.lidar_dem = kwargs['lidar_dem']
+        self.sfm_dem = kwargs['sfm_dem']
+        self.shape_file = kwargs['shape_file']
+
+    @property
+    def base_path(self):
+        return self._base_path
+
+    @base_path.setter
+    def base_path(self, path):
+        self._base_path = self.check_path(os.path.join(path, ''))
+
+    @property
+    def lidar_dem(self):
+        return self._lidar_dem
+
+    @lidar_dem.setter
+    def lidar_dem(self, path):
+        self._lidar_dem = self.check_path(path)
+
+    @property
+    def sfm_dem(self):
+        return self._sfm_dem
+
+    @sfm_dem.setter
+    def sfm_dem(self, path):
+        self._sfm_dem = self.check_path(path)
+
+    @property
+    def output_path(self):
+        return self._output_path
+
+    @output_path.setter
+    def output_path(self, path):
+        output_path = os.path.join(self.base_path, path, '')
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        self._output_path = output_path
+
+    @property
+    def shape_file(self):
+        return self._shape_file
+
+    @shape_file.setter
+    def shape_file(self, path):
+        if path is not None:
+            self._shape_file = self.check_path(path)
+
+    def check_path(self, path):
+        if hasattr(self, 'base_path'):
+            _path = os.path.join(self.base_path, path)
+        else:
+            _path = path
+        if not os.path.exists(_path):
+            print('** ERROR **\n  Given file or path missing: ' + _path + '\n')
+            sys.exit()
+        return _path
+
+    @staticmethod
+    def crop_to_shape(raster_file, shape_file):
+        output_file = raster_file.replace('.tif', '_cropped.tif')
+
+        if not os.path.exists(output_file):
+            print('Cropping raster:\n   ' + raster_file +
+                  '\nto shape:\n   ' + shape_file + '\n')
+            gdal.Warp(
+                output_file, raster_file,
+                format='GTiff', dstAlpha=True, cropToCutline=True,
+                cutlineDSName=shape_file
+            )
+
+        return output_file
+
+    def prepare(self):
+        if self.shape_file:
+            self.lidar_dem = self.crop_to_shape(self.lidar_dem, self.shape_file)
+            self.sfm_dem = self.crop_to_shape(self.sfm_dem, self.shape_file)
+
+    def file_args(self):
+        return dict(
+            lidar=RasterFile(self.lidar_dem),
+            sfm=RasterFile(self.sfm_dem),
+            output_path=self.output_path
+        )
--- raster_compare/plot_all.py
+++ raster_compare/plot_all.py
@@ -1,10 +1,8 @@
 import argparse
-import os
-import sys
 
-from base.raster_file import RasterFile
-from area_plot import AreaPlot
 from area_differences import AreaDifferences
+from area_plot import AreaPlot
+from base.raster_compare import RasterCompare
 from histograms import Histogram
 from regression import Regression
 
@@ -52,37 +50,21 @@ parser.add_argument(
 if __name__ == '__main__':
     arguments = parser.parse_args()
 
-    base_path = os.path.join(arguments.base_path)
-
-    lidar_file = RasterFile(os.path.join(base_path, arguments.lidar_dem))
-    sfm_file = RasterFile(os.path.join(base_path, arguments.sfm_dem))
-
-    output_path = os.path.join(base_path, arguments.output_path, '')
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-
-    shape_file = os.path.join(base_path, arguments.shape_file)
-    if arguments.shape_file and os.path.exists(shape_file):
-        lidar_file.crop_to_shape(shape_file)
-        sfm_file.crop_to_shape(shape_file)
-    elif arguments.shape_file:
-        print('ERROR - Shapefile not found:\n   ' + shape_file)
-        sys.exit()
-
-    args = dict(lidar=lidar_file, sfm=sfm_file, output_path=output_path)
+    comparison = RasterCompare(**vars(arguments))
+    comparison.prepare()
 
     if arguments.plots:
-        area_plot = AreaPlot(**args)
+        area_plot = AreaPlot(**comparison.file_args())
         [area_plot.plot(attr) for attr in AreaPlot.TYPES]
         del area_plot
 
-        area_difference = AreaDifferences(**args)
+        area_difference = AreaDifferences(**comparison.file_args())
         [area_difference.plot(attr) for attr in AreaDifferences.TYPES]
         del area_difference
 
-        histogram = Histogram(**args)
+        histogram = Histogram(**comparison.file_args())
         [histogram.plot(attr) for attr in Histogram.TYPES]
         del histogram
 
     if arguments.regression:
-        Regression(**args).scatter_plots()
+        Regression(**comparison.file_args()).scatter_plots()
