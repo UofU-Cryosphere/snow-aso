915a29d6ae69c05f4c21c137c33633a9b2a8a82b
Joachim Meyer <j.meyer@utah.edu>
2018-09-06 15:50:41 -0600

    Raster compare - Extend Regression analysis.
    Add:
    - differences vs sfm (slope, aspect, elevation)
    - Lidar vs SfM elevations

--- raster_compare/regression.py
+++ raster_compare/regression.py
@@ -17,32 +17,68 @@ class Regression(PlotBase):
                 self.raster_difference.elevation.filled(np.NaN).ravel()
         return self._elevation_differences
 
-    def scatter_plots(self):
-        for variable in self.TYPES:
-            self.print_status(variable)
+    def scatter_plot(self, x, y, **kwargs):
+        plt.figure()
+        plt.scatter(x, y, s=0.5)
+        plt.xlabel(kwargs['xlabel'])
+        plt.ylabel(kwargs['ylabel'])
+        plt.savefig(
+            self.output_path + '/{0}_scatter_plot.png'.format(kwargs['name']),
+            **self.output_defaults()
+        )
 
-            plt.figure()
-            plt.scatter(
+    def plot_lidar_vs_sfm(self):
+        self.scatter_plot(
+            self.sfm.elevation,
+            self.lidar.elevation,
+            name='lidar_vs_sfm',
+            xlabel='SfM',
+            ylabel='Lidar',
+        )
+
+    def plot_difference_vs_source(self, source, name):
+        for variable in self.TYPES:
+            name = name + '_' + variable
+            self.print_status(name)
+            self.scatter_plot(
                 self.elevation_differences,
-                getattr(self.lidar, variable),
-            )
-            plt.xlabel('Difference in Elevation')
-            plt.ylabel('Lidar: ' + str(variable))
-            plt.savefig(
-                self.output_path + '/{0}_scatter_plot.png'.format(variable),
-                **self.output_defaults()
+                getattr(source, variable),
+                name=name,
+                xlabel='Difference in Elevation',
+                ylabel=name + ': ' + str(variable),
             )
 
-    def fit_models(self):
+    def plot_all(self):
+        self.plot_lidar_vs_sfm()
+        self.plot_difference_vs_source(self.lidar, 'Lidar')
+        self.plot_difference_vs_source(self.sfm, 'SfM')
+
+    @staticmethod
+    def fit_model(y, x, name):
+        print('\n** OLS for: ' + name + ' **\n')
+        model = sm.OLS(y, x, missing='drop',).fit()
+        print(model.summary())
+
+    def fit_difference_vs_source(self, source, name):
         for variable in self.TYPES:
-            print('\n** OLS for: ' + str(variable) + ' **\n')
-            model = sm.OLS(
+            self.fit_model(
                 self.elevation_differences,
-                getattr(self.lidar, variable).ravel(),
-                missing='drop',
-            ).fit()
-            print(model.summary())
+                getattr(source, variable).ravel(),
+                name + ': ' + variable,
+            )
+
+    def fit_lidar_vs_sfm(self):
+        self.fit_model(
+            self.lidar.elevation.ravel(),
+            self.sfm.elevation.ravel(),
+            'Lidar vs. SfM',
+        )
+
+    def fit_all(self):
+        self.fit_lidar_vs_sfm()
+        self.fit_difference_vs_source(self.lidar, 'Lidar')
+        self.fit_difference_vs_source(self.sfm, 'SfM')
 
     def run(self):
-        self.scatter_plots()
-        self.fit_models()
+        self.plot_all()
+        self.fit_all()
