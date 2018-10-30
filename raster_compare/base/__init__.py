b469d7fd9c64e758100e6f41489d850b81555bf9
Joachim Meyer <j.meyer@utah.edu>
2018-10-30 08:37:16 -0600

    Raster compare - Add MedianAbsoluteDeviation class.
    Class to calculate the elevation difference based on Median absolute
    deviance values and the normalized value based on Höhle & Höhle, 2009.

--- raster_compare/base/__init__.py
+++ raster_compare/base/__init__.py
@@ -1,10 +1,12 @@
-from .rgb_average import RgbAverage
+from .median_absolute_deviation import MedianAbsoluteDeviation
 from .plot_base import PlotBase
 from .raster_compare import RasterCompare
 from .raster_difference import RasterDifference
 from .raster_file import RasterFile
+from .rgb_average import RgbAverage
 
 __all__ = [
+    'MedianAbsoluteDeviation',
     'RgbAverage',
     'PlotBase',
     'RasterCompare',
---
+++ raster_compare/base/median_absolute_deviation.py
@@ -0,0 +1,41 @@
+import math
+
+import numpy as np
+
+
+class MedianAbsoluteDeviation(object):
+    """
+    Median Absolute Deviation
+    """
+
+    MAD_CONSTANT = 1.4826
+
+    def __init__(self, data):
+        self._data = data
+        self._data_mean = np.median(self.data)
+        self._percentiles = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_median(self):
+        return self._data_mean
+
+    def absolute_difference(self, a):
+        return math.fabs(a - self.data_median)
+
+    def normalized(self):
+        """
+        NMAD from Höhle & Höhle, 2009
+        """
+        absolute_difference = np.vectorize(self.absolute_difference)
+        return self.MAD_CONSTANT * np.median(absolute_difference(self.data))
+
+    def percentile(self, percent):
+        if str(percent) not in self._percentiles:
+            absolute_difference = np.vectorize(self.absolute_difference)
+            self._percentiles[str(percent)] = \
+                np.percentile(absolute_difference(self.data), percent)
+        return self._percentiles[str(percent)]
