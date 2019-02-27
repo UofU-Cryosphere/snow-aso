31601c7ac51434cd2fb5757036c4b6b570c2d93d
Joachim Meyer <j.meyer@utah.edu>
2019-02-27 07:48:48 -0700

    Refactor SideBySide plot - Move colorbar to dedicated axes.
    Move the colorbar to dedicated axes that fixes the disproportionally
    display of the two DEMs. Also move some logic out of the plot method to
    enable overwriting in sub classes.

--- raster_compare/plots/side_by_side.py
+++ raster_compare/plots/side_by_side.py
@@ -8,47 +8,62 @@ from .plot_base import PlotBase
 class SideBySide(PlotBase):
     OUTPUT_FILE = '{0}{1}_comparison.png'
 
-    def plot(self, raster_attr):
-        self.print_status(str(raster_attr))
+    COLOR_MAP = 'jet'
 
-        figure, (ax1, ax2) = plt.subplots(ncols=2)
-        figure.set_size_inches(10, 5)
+    @staticmethod
+    def setup_plot():
+        figure, axes = plt.subplots(
+            figsize=(12, 4), ncols=3,
+            gridspec_kw={'width_ratios': [1, 1, 0.05], 'wspace': 0.05}
+        )
 
-        axes = (ax1, ax2)
-        ax1.get_shared_x_axes().join(ax1, ax2)
-        ax1.get_shared_y_axes().join(ax1, ax2)
+        axes[0].get_shared_y_axes().join(*axes[:2])
 
-        for n, ax in enumerate(axes):
+        for n, ax in enumerate(axes[:2]):
             ax.set_facecolor('0.9')
 
-        self.add_hillshade_background(ax1, self.lidar)
-        self.sfm.join_masks('hill_shade', getattr(self.lidar, raster_attr))
-        self.add_hillshade_background(ax2, self.sfm)
+        return axes
 
-        im_opts = dict(
-            cmap=cm.get_cmap('jet'),
+    def im_opts(self, raster_attr):
+        return dict(
+            cmap=cm.get_cmap(self.COLOR_MAP),
             alpha=0.3,
             vmin=self.min_for_attr(raster_attr),
             vmax=self.max_for_attr(raster_attr),
         )
 
+    @staticmethod
+    def add_colorbar(cax, data):
+        return plt.colorbar(data, cax=cax)
+
+    def plot(self, raster_attr):
+        self.print_status(str(raster_attr))
+
+        (ax1, ax2, cax) = self.setup_plot()
+
+        self.add_hillshade_background(ax1, self.lidar)
+        self.sfm.join_masks('hill_shade', getattr(self.lidar, raster_attr))
+        self.add_hillshade_background(ax2, self.sfm)
+
         ax1.imshow(
             getattr(self.lidar, raster_attr),
             extent=self.lidar.extent,
-            **im_opts
+            **self.im_opts(raster_attr)
         )
-        ax1.set_title(PlotBase.LIDAR_LABEL, **PlotBase.title_opts())
+        ax1.set_title(PlotBase.LIDAR_LABEL)
 
         self.sfm.join_masks(raster_attr, getattr(self.lidar, raster_attr))
         image = ax2.imshow(
-            getattr(self.sfm, raster_attr), extent=self.sfm.extent, **im_opts
+            getattr(self.sfm, raster_attr),
+            extent=self.sfm.extent,
+            **self.im_opts(raster_attr)
         )
         ax2.set_yticklabels([])
-        ax2.set_title(PlotBase.SFM_LABEL, **PlotBase.title_opts())
+        ax2.set_title(PlotBase.SFM_LABEL)
 
-        self.insert_colorbar(plt, ax2, image, self.SCALE_BAR_LABEL[raster_attr])
+        cbar = self.add_colorbar(cax, image)
+        cbar.set_label(self.SCALE_BAR_LABEL[raster_attr])
 
-        plt.tight_layout()
         plt.savefig(
             self.OUTPUT_FILE.format(self.output_path, raster_attr),
         )
