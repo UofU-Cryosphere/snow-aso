f34ac0c2ae347765ed99defc537774877ae5fe12
Joachim Meyer <j.meyer@utah.edu>
2019-04-26 11:10:03 -0600

    Change setup.py to install images_time_table as package.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='raster-compare',
+    name='snow-aso',
     version='0.1',
     packages=[
-        'images_time_table.base', 'raster_compare.base', 'raster_compare.plots',
+        'images_time_table.base',
     ],
     url='https://github.com/UofU-Cryosphere/snow-aso',
     author='Joachim Meyer',
     author_email='j.meyer@utah.edu',
     description='Raster comparison tools',
     install_requires=[
-        'gdal', 'matplotlib', 'numpy', 'pdal', 'pandas',
-        'opencv-python', 'pillow', 'statsmodels'
+        'numpy', 'pandas'
     ]
 )
