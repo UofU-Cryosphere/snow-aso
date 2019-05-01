5a000a93e9ecce3742af406dcc2582850e4feb6f
Joachim Meyer <jomey@users.noreply.github.com>
2019-05-01 09:33:38 -0600

    Update setup.py description.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/UofU-Cryosphere/snow-aso',
     author='Joachim Meyer',
     author_email='j.meyer@utah.edu',
-    description='Raster comparison tools',
+    description='CHPC tools to process imagery and point clouds',
     install_requires=[
         'numpy', 'pandas'
     ]
